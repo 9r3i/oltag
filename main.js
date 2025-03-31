e45b63cd4eb129c93182795da65bb8997e049200
9r3i <luthfie@y7mail.com>
2025-04-01 04:01:29 +0700

    add catch

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ async function main() {
   url='https://hotelbandara.com/api/online/?i=9300';
   while(true){
     counter++;
-    let res=await fetch(url),
-    text=await res.text();
-    console.log(counter+' '+text);
+    let res=await fetch(url)
+      .then(r=>r.text())
+      .catch(e=>JSON.stringify(e));
+    console.log(counter+' '+res);
     await sleep(10);
   }
 }
